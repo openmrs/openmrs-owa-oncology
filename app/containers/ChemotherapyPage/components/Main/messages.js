2aa071ee5458a6d1b2024c3220cb5fadbaa5ada8
Oleksandr Sabov <sabov@de.ibm.com>
2018-07-29 12:55:18 -0400

    Add AdministrateForm template

---
+++ app/containers/ChemotherapyPage/components/Main/messages.js
@@ -0,0 +1,14 @@
+/*
+ * Main Messages
+ *
+ * This contains all the text for the Main component.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'app.components.Main.header',
+    defaultMessage: 'This is the Main component !',
+  },
+});
