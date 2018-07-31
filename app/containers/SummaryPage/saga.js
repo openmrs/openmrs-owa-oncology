eaabcee19a77032d71c160ece6a213fe35fb53d1
Pravesh Patel <pravesh@us.ibm.com>
2018-07-30 21:25:15 -0400

    added file to hold hardcoded UUIDs

--- app/containers/SummaryPage/saga.js
+++ app/containers/SummaryPage/saga.js
@@ -1,6 +1,8 @@
 import { put, call, select, takeLatest } from 'redux-saga/effects';
 import { POST_CHEMO_ORDER } from 'containers/OrderPage/constants';
 import request from 'utils/request';
+import {OGR_CHEMO_PARENT_SET, OGR_PREMEDICATION, OGR_CHEMOTHERAPY, OGR_POSTMEDICATION,
+  OGAT_CYCLE_NUMBER, OGAT_NUM_CYCLES, OGAT_CYCLE_LENGTH, OGAT_CYCLE_LENGTH_UNIT} from '../../conceptMapping.json';
 
 import {
   // postEncounterFailureAction,
@@ -27,12 +29,44 @@ export function* postChemoOrder(action) {
     yield put(postEncounterSuccessAction(encounterResponse));
 
     const regimenList = yield select(makeSelectRegimenList());
-    const orderset = regimenList.results[action.orderInfo.orderIndex].uuid;
+    const orderSet = regimenList.results[action.orderInfo.orderIndex].uuid;
 
     const orderGroup = {
       encounter: encounterResponse.uuid,
-      orderset,
-      // orders:[], 
+      orderSet,
+      orderGroupReason: OGR_CHEMO_PARENT_SET,
+      nestedOrderGroups: [
+        {
+          orderGroupReason: OGR_PREMEDICATION,
+          orders:[],
+        },
+        {
+          orderGroupReason: OGR_CHEMOTHERAPY,
+          orders:[],
+        },
+        {
+          orderGroupReason: OGR_POSTMEDICATION,
+          orders:[],
+        },     
+      ],
+      attributes: [
+        {
+          attributeType: OGAT_CYCLE_NUMBER,
+          value:'',
+        },
+        {
+          attributeType: OGAT_NUM_CYCLES,
+          value:'',
+        },
+        {
+          attributeType: OGAT_CYCLE_LENGTH,
+          value:'',
+        },
+        {
+          attributeType: OGAT_CYCLE_LENGTH_UNIT,
+          value: '',
+        },
+      ],
     };
 
     const orderGroupResponse = yield call(request, requestUrlOrderGroup, {
